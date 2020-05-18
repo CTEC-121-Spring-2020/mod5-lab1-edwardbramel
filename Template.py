24b5a7e1abd9d6f446380424e2bb979d3428f920
Eddy Bramel <e.lamanna-bramel@students.clark.edu>
2020-05-18 10:42:38 -0700

    happppy happpy birthday

--- Template.py
+++ Template.py
@@ -1,7 +1,7 @@
 """
 CTEC 121
-<your name>
-<assignment/lab name>
+Edward Lamanna
+mod5 lab 1
 <assignment/lab description
 """
 
@@ -11,7 +11,56 @@ Process: description of what function does
 Output: return value and description
 """
 
-def main():
-    # code here
 
-main()    
+def main1():
+    print()
+    print("Happy birthday to you!")
+    print("Happy birthday to you!")
+    print("Happy birthday, dear Fred...")
+    print("Happy birthday to you!")
+    print()
+
+
+# remove obvious dups
+""" IPO happy()
+Input(s): none
+Process: prints/outputs chorus of happy birthday song
+Output: prints to terminal screen
+"""
+
+
+def happy():
+    print("Happy birthday to you!")
+
+
+def main2():
+    print()
+    happy()
+    happy()
+    print("Happy birthday to you!")
+    happy()
+    print()
+
+
+# generALIZE FOR ANY PERSON
+""" IPO happy()
+Input(s): none
+Process: prints/outputs verse line of happy birthday song
+Output: prints to terminal screen
+"""
+
+
+def happyBDay(name):
+    print("happy birthday, dear ", name, "...", sep="")
+
+
+def main3():
+    print()
+    happy()
+    happy()
+    happyBDay("Joe mama")
+    happy()
+    print()
+
+
+main3()
